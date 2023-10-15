d1ccbcb6dab0f92a09697798cb3406ec06271a5f
Matthew Wang <wang.mat@northeastern.edu>
2023-10-15 13:40:49 -0400

    updated FormScreen

--- FormScreen.js
+++ FormScreen.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function FormScreen() {
     return (
         <div>
-            <Text>Form Screen</Text>
+            <Text>Form Screen!!</Text>
             <StatusBar style="auto" />
         </div>
     );
