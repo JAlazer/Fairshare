6008dcdd5b492f3cc50d054f213c07e000e09f14
Matthew Wang <wang.mat@northeastern.edu>
2023-10-15 13:48:02 -0400

    stuff

--- FormScreen.js
+++ FormScreen.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function FormScreen() {
     return (
         <div>
             <Text>Form Screen!!</Text>
+            <Button>This is a button</Button>
             <StatusBar style="auto" />
         </div>
     );
