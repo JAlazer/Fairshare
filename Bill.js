4f17a988606a7cec205d7eaac365785f113c8d65
Johan <almanzar.j@northeastern.edu>
2023-10-28 19:59:53 -0400

    Update 1

---
+++ Bill.js
@@ -0,0 +1,10 @@
+import { View } from "react-native"
+import React, {useState} from "react";
+
+const Bill = () => {
+    return (
+        <View>
+            <Text>Bill Screen!</Text>
+        </View>
+    );
+}
