0987a72b367b394ab30b133c0f8f702b9b6bda80
Matthew Wang <wang.mat@northeastern.edu>
2023-10-29 13:52:36 -0400

    working changes

--- App.jsx
+++ App.jsx
@@ -1,16 +1,26 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import 'react-native-gesture-handler';
-import WelcomeScreen from './components/WelcomeScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import "react-native-gesture-handler";
+import WelcomeScreen from "./components/WelcomeScreen";
+import FormScreen from "./FormScreen";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer >
+    <NavigationContainer>
       <Stack.Navigator intialRouteName="WelcomeScreen">
-      {/*change headerShown to false to remove top of screen*/}
-        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={{ headerShown: false }}/>
+        {/*change headerShown to false to remove top of screen*/}
+        <Stack.Screen
+          name="WelcomeScreen"
+          component={WelcomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="FormScreen"
+          component={FormScreen}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
