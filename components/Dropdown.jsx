4b2f44e53953b24edba3c92607b7819039b3901d
pmoise <petercmoise@gmail.com>
2023-12-02 20:49:51 -0500

    Done

--- components/Dropdown.jsx
+++ components/Dropdown.jsx
@@ -1,15 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { IndexPath, Layout, Select, SelectItem } from '@ui-kitten/components';
 
-const SelectMultiSelectShowcase = ({people, food}) => {
+const SelectMultiSelectShowcase = ({people, food, onUpdate2dArray}) => {
   const [selectedIndexes, setSelectedIndexes] = useState(Array(food.length).fill([]));
+  const [assignList, setAssignList] = useState([]);
   const onSelect = (index, foodIndex) => {
     const updatedIndexes = [...selectedIndexes];
     updatedIndexes[foodIndex] = index;
     setSelectedIndexes(updatedIndexes);
   };
+  
 
+  useEffect(() => {
+    const updatedAssignList = selectedIndexes.map((selectedIndexArr) =>
+      selectedIndexArr.map((selectedIndex) => people[selectedIndex - 1]), // Adjusting index to start from 0
+    );
+    //setAssignList(updatedAssignList);
+    onUpdate2dArray(updatedAssignList)
+    //console.log(assignList);
+    //console.log(updatedAssignList)
+  }, [selectedIndexes]);
 
   return (
     <Layout style={styles.container} level='3'>
